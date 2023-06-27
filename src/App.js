5915147dad084e56c6df10883d049fa8f5780c41
mohamed2233996 <medomano771@gmail.com>
2023-06-27 16:07:02 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div className="App">
+      <div className="title">
+        <h1>Medo App</h1>
+        <p>start with us</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
